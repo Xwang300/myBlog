9c82834356af5b24c9202bc18d79edf6ac13ac17
wangxin <www7718975@hotmail.com>
2020-07-08 21:40:44 +0800

    remove useless import

--- src/main/java/com/cass/controller/admin/AdminPictureController.java
+++ src/main/java/com/cass/controller/admin/AdminPictureController.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 /**
